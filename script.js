85262d715aa458ab2ef6fc4c3182e9d5f74061cd
MatiasLGmdp <matiaslgonzalez2013@gmail.com>
2022-08-18 09:16:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('hello!');
+
+var JSONObj = [
+  { prenda: 'Sweter', Talle: 'S', Color: 'Azul' },
+  { prenda: 'Jean', Talle: 'M', Color: 'Blanco' },
+  { prenda: 'Remera', Talle: 'L', Color: 'Rojo' },
+];
+
+console.log(JSONObj);
